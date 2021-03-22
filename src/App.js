116d10589cfc629f20341dd8fbdb5c519db1e0ed
liyahshaikh <liyahshaikh965@gmail.com>
2021-03-22 20:55:37 +0530

    Made the product component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
     <div className="app">
       
       <Header />
-      {/* Home */}
+      <Home />
     </div>
   );
 }
