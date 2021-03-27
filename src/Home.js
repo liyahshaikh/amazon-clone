118f43206e0c7d6e93511a71a69c0a8158e9505a
liyahshaikh <liyahshaikh965@gmail.com>
2021-03-27 23:54:33 +0530

    Added props to products

--- src/Home.js
+++ src/Home.js
@@ -9,8 +9,18 @@ function Home() {
                 alt="Amazon Banner" />
             </div>
             <div className="home__row">
-                <Products />
-                <Products />
+                <Products 
+                id={197348342783}
+                title='The lean startup'
+                 price={29.99} 
+                 image='https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg'
+                rating={5} />
+                <Products 
+                id={197346652783}
+                image='https://m.media-amazon.com/images/I/61Mo9G7+EkL._AC_UL320_.jpg'
+                title='Amazon Basics 14-Piece Kitchen Knife Set with High-Carbon Stainless-Steel Blades and Pine Wood Block'
+                price={24.99}
+                rating={4} />
                 
                 
             </div>
--- src/Products.js
+++ src/Products.js
@@ -2,23 +2,27 @@ import React from 'react'
 import "./Products.css"
 import StarRateIcon from '@material-ui/icons/StarRate'
 
-function Products() {
+function Products({id,title, image,price, rating}) {
     return (
         <div className="product">
             <div className="product__info">
-                <p>The lean startup</p>
+                <p>{title}</p>
                 <div className="product__price">
                 <small className="page__sign">$</small>
-                <strong>19.99</strong>
+                <strong>{price}</strong>
 
                 <div className="product__rating">
-            <StarRateIcon className="product__star"/>
-            </div>
+                    {Array(rating)
+                    .fill()
+                    .map((_,i) => (
+                        <StarRateIcon className="product__star"/>
+                    ))}
+                 </div>
             </div>
             
             </div>
             
-            <img src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg" alt="" className="product__img"/>
+            <img src={image} alt="" className="product__img"/>
             <button className="product__addToBasket">Add To Basket</button>
         </div>
     )
